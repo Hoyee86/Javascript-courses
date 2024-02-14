5d58149065c992a6db293b50a208159433391257
Hoyee86git config --global user.name Hoyee86 <azeezabdullahi264@gmail.com>
2024-02-14 00:43:30 -0800

    my first commit

---
+++ Variable/script.js
@@ -0,0 +1,118 @@
+// Variables.
+// let
+//const
+
+let message = "Hello Eef";
+message = "Hello Zainab";
+console.log(message);
+
+
+const greeting = "Good morning my Neighbor"
+console.log(greeting)
+
+
+
+const _name$ = "Al Fulani ileya 🏠";
+console.log(_name$);
+
+
+console.log(message, greeting, _name$)
+
+
+
+let studentName = "Azeez Abdullah Adebayo";
+studentName = "Good Afternoon";
+console.log(studentName);
+
+const _var = "Hello!";
+const var$ = "My Neighbour";
+const var3 = "How are you doind?";
+
+console.log(_var, var$, var3)
+
+
+// DATA TYPE
+// String - Number- BigInt- Symbol-Undefined- Boolean - Null - typeof  *** object - Array - Date
+
+//string
+//string string -''
+//Double string-""
+//Back ticks-``
+
+
+const singleQoute = 'Who am I?';
+const doubleQoute = "Who are you?";
+
+console.log(singleQoute);
+console.log(doubleQoute);
+
+
+let name = "Team Lead";
+const backTick = `Hello ${name}`;
+console.log(backTick);
+
+
+let newName = 'Toyib';
+let favouriteDrink = 'Bobo';
+const details = `My name is ${newName}, and my favorite drink is ${favouriteDrink}`;
+
+console.log(details);
+
+// class work
+const numberOne = 45;
+const numberTwo =10;
+
+const add = numberOne + numberTwo;
+console.log(add)
+
+const sub = numberOne - numberTwo;
+console.log(sub)
+
+const div = numberOne / numberTwo;
+console.log(div)
+
+const mul = `${numberOne * numberTwo}`;
+console.log(mul)
+
+
+const numberThree = '10';
+const numberFour = '14';
+console.log(numberThree+numberFour)
+
+console.log(10+'15');
+
+
+// Boolean - True / False
+
+const isBeautiful = true;
+console.log(typeof isBeautiful)
+
+
+if (isBeautiful) {
+    console.log("This woman is beautiful")
+} else {
+    console.log('This woman is ugly')   
+}
+
+
+//Null
+let age = null;
+console.log(age);
+
+// undefined
+
+let x;
+console.log(x);
+
+// symbol
+const value1 = Symbol("hello");
+const value2 = Symbol("hello");
+console.log(value1.description);
+
+
+
+
+
+
+
+
