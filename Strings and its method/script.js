2bc0fc4d0911c21064b79a0c222897a1ad87924d
Hoyee86git config --global user.name Hoyee86 <azeezabdullahi264@gmail.com>
2024-02-20 15:58:05 +0100

    update

---
+++ Strings and its method/script.js
@@ -0,0 +1,84 @@
+// Types of strings
+
+// single qoute ('')
+const world = 'i am a boy'
+
+// double qoute ("")
+const words = "i am a boy"
+
+// backticks (``)
+// const worlds = `i am a boy`
+
+
+// // console.log(world, words, world)
+
+
+const age = 12
+const school = 'DLT'
+const name = 'Eef'
+
+
+const sentence = `my name is ${name}, i am ${age} years old I go to ${school} school`
+console.log(sentence)
+
+
+const math = "2 + 2"
+
+const mathematics = `${2+ 2}`
+
+const line = `1
+2
+3
+4
+5`
+
+console.log(line)
+
+
+const numerals = `Hello I'm Abdullahi and I am from "Osun state"`
+console.log(numerals)
+
+
+// checking the length of a string
+
+const named = "Hayzed"
+
+console.log(named.length)
+
+console.log(name[0])
+
+
+// changing the case of string
+
+const word = "I am a boy"
+const worLd = "SHE IS A GIRL "
+
+console.log(word.toUpperCase())
+console.log(world.toLowerCase())
+
+// checking for Substring
+
+const man = "I love coding and my favourite language is Javascript"
+
+
+const caps = man[31].toUpperCase()
+console.log(caps)
+
+const final = man.indexOf('Javascript')
+console.log(final)
+
+ const check = man.includes('favourite')
+ console.log(check)
+
+
+ const starting = man.startsWith('I')
+ console.log(starting)
+
+ const ending = man.endsWith('t')
+ console.log(ending)
+
+
+
+
+
+
