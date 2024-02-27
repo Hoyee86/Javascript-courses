942a9f1a0998e8c9b035ad2b7e77b0b9b266d90a
Hoyee86git config --global user.name Hoyee86 <azeezabdullahi264@gmail.com>
2024-02-27 12:53:11 +0100

    updating

--- ARRAY IN DETAILS/script.js
+++ ARRAY IN DETAILS/script.js
@@ -1,265 +1,267 @@
-// const cohorts = [
-//     "Soliu", 
-//     "Abdullahi", 
-//     "Hayzed", 
-//     "Bode", 
-//     "sir kenny", 
-//     "Basheera", 
-//     "Br. Tunmise", 
-//     "Captain",]
-//     console.log(cohorts)
+const cohorts = [
+    "Soliu", 
+    "Abdullahi", 
+    "Hayzed", 
+    "Bode", 
+    "sir kenny", 
+    "Basheera", 
+    "Br. Tunmise", 
+    "Captain",]
+    console.log(cohorts)
 
 
-//     const replaceEl = (cohorts[1] = "Tolu")
+    const replaceEl = (cohorts[2] = "Tolu")
     
-// // To add another element in the array
-//     cohorts[8] = "Bimpe"
-//     cohorts[9] = "OLOYE"
+// To add another element in the array
+    cohorts[8] = "Bimpe"
+    cohorts[9] = "OLOYE"
 
 
 
 
 //     // class-work (loop for cohorts)
 
-//     // ES5
+    // ES5
 
-//     for (let i = 0; i < cohorts.length; i++) {
-//         console.log(i, cohorts[i])
-//     }
+    for (let i = 0; i < cohorts.length; i++) {
+        console.log(i, cohorts[i])
+    }
 
-//     // ES6
+    // ES6
 
-//     for(const hoye of cohorts) {
-//           console.log(hoye);
-//     }
+    for(const hoye of cohorts) {
+          console.log(hoye);
+    }
 
 
 //     // EXAMPLE
 
 
-//     const nested = [[1,2], [3,4], [5,6]]
-//     const newArray = nested[2]
-//     console.log(newArray);
+    const nested = [[1,2], [3,4], [5,6]]
+    const newArray = nested[2]
+    console.log(newArray);
 
 
 
-//     const object = {
-//          name : "John",
-//          age : 60,
-//     }
+    const object = {
+         name : "John",
+         age : 60,
+    }
 
-//     console.log(object)
+    console.log(object)
 
 
-//     const arr = [
-//         "Banana",
-//         10000,
-//         {
-//             name : "Bola blu",
-//             age : 80
-//         },
+    const arr = [
+        "Banana",
+        10000,
+        {
+            name : "Bola blu",
+            age : 80
+        },
 
-//         function name () {
-//             alert("boooooooooooooooooooo")
-//         }
-//     ]
+        function name () {
+            alert("boooooooooooooooooooo")
+        }
+    ]
 
-//     console.log(typeof arr)
+    console.log(typeof arr)
 
 //     // ************PUSH METHOD*************/
 
 //     // ARRAY-PUSH => The push method returns the new array length:
 //     // it add new element to the end of an array
 
-//     const students = ["Yemi", "sola", "Grace", "Mary", "Femi", "Bola"]
-//     console.log(students);
-//     const pushed = students.push("Babe", "Rofiya", "Rhamat")
-//     console.log(pushed);
-//     console.log(students)
+    const students = ["Yemi", "sola", "Grace", "Mary", "Femi", "Bola"]
+    console.log(students);
+    const pushed = students.push("Babe", "Rofiya", "Rhamat")
+    console.log(pushed);
+    console.log(students)
 
 
 
-//     // POP METHOD : This method is used to remove last data from an array
+//     // POP METHOD : This method is used to remove last
+// data from an array
 
-//     const numbers = [3, 4, 5, 6, 7, 8, 9];
-//     console.log(numbers)
+    const numbered = [3, 4, 5, 6, 7, 8, 9];
+    console.log(numbered)
 
-//     const popped = numbers.pop()
-//     console.log(popped);
-//     console.log(numbers)
+    const popped = numbered.pop()
+    console.log(popped);
+    console.log(numbered)
 
 
 //      //***** SHIFT METHOD () ************** : This is used to remove first
 //      // element of the an array
 
-//      const colors = ["Violent", "Red", "Blue", "Black"]
-//      console.log(colors)
-//      const shift = colors.shift()
-//      console.log(colors)
+     const colors = ["Violent", "Red", "Blue", "Black"]
+     console.log(colors)
+     const shift = colors.shift()
+     console.log(colors)
     
 //     //***** UNSHIFT METHOD () ************** : This is used to add
 //     //  to the first element of an array
 
 
-//     const months = ["March", "July", "september", "January"];
-//     console.log(months)
-//     const newMonths = months.unshift("Febuary")
-//     console.log(newMonths)
-//     console.log(months)
+    const months = ["March", "July", "september", "January"];
+    console.log(months)
+    const newMonths = months.unshift("Febuary")
+    console.log(newMonths)
+    console.log(months)
 
 
 //     //***** SPLICE METHOD () ************** : This array method is used to return 
 //     // new element to an array with an index position and it also helps to
 //     // remove element from an array.   
 
-//     const countries = ["Kenya", "Malaysia", "Canada", "Qatar", "Nigeria"];
-//     console.log(countries)
-//     countries.splice(3, 0, "Egypt", "Ghana", "Morroco")
-//     console.log(countries)
+    const countries = ["Kenya", "Malaysia", "Canada", "Qatar", "Nigeria"];
+    console.log(countries)
+    countries.splice(3, 0, "Egypt", "Ghana", "Morroco")
+    console.log(countries)
 
 
-//     const wears = ["Gucci", "Versace", "D&G", "Nike", "LV"]
-//     console.log(wears)
-//     wears.splice(3,)
-//     console.log(wears)
+    const wears = ["Gucci", "Versace", "D&G", "Nike", "LV"]
+    console.log(wears)
+    wears.splice(3,)
+    console.log(wears)
 
 
-//     const drinks = ["coke", "viju", "Gin", "Exotic", "Bobo"]
-//     console.log(drinks)
-//     drinks.splice(2,1)
-//     console.log(drinks)
+    const drinks = ["coke", "viju", "Gin", "Exotic", "Bobo"]
+    console.log(drinks)
+    drinks.splice(2,1)
+    console.log(drinks)
 
 
 //     //***** SLICE METHOD () ************** : This method returns selected elements in an array
 //     // as new array, and it  does not change the original array 
 
 
-//     const men = ["Tunmise", "Kenny", "Soliu", "Bode", "Abee", "Hayzed"]
-//     console.log(men)
+    const men = ["Tunmise", "Kenny", "Soliu", "Bode", "Abee", "Hayzed"]
+    console.log(men)
     
 
-//     const newString = "Naheem"
-//     console.log(newString.length)
-//     const newSlice = newString.slice(2,5)
-//     console.log(newSlice)
+    const newString = "Naheem"
+    console.log(newString.length)
+    const newSlice = newString.slice(2,5)
+    console.log(newSlice)
 
 
-//     const baddestMen = men.slice(2, 5)
-//     console.log(baddestMen)
+    const baddestMen = men.slice(2, 5)
+    console.log(baddestMen)
 
 
-//     const coolestAmong = men.slice(4)
-//     console.log(coolestAmong)
+    const coolestAmong = men.slice(4)
+    console.log(coolestAmong)
 
 
 //  //***** INCLUDE METHOD () ************** 
 
 
-//  const checkMen = men.includes("Abee")
-//  console.log(checkMen)
+ const checkMen = men.includes("Abee")
+ console.log(checkMen)
 
-//  const checkMEN = men.includes("Bash")
-//  console.log(checkMEN)
+ const checkMEN = men.includes("Bash")
+ console.log(checkMEN)
 
-//   //***** SORT METHOD () ************** : This method is used to re-arra
-//   // nge element in an array with the help of call back function
-//   // () => both ascending and descending orders.
+  //***** SORT METHOD () ************** : This method is used to re-arra
+  // nge element in an array with the help of call back function
+  // () => both ascending and descending orders.
 
-//   men.sort();
-//   console.log(men)
-
-
-//   const alpha = ["G", "F", "K", "A", "B", "Z"];
-//   console.log(alpha)
-
-//   alpha.sort();
-//   console.log(alpha)
+  men.sort();
+  console.log(men)
 
 
-//   const numbs = [9, 7, 6, 8, 15, 25, 35, 99, 5, 1, 3, 2, 4]
-//   console.log(numbs)
+  const alpha = ["G", "F", "K", "A", "B", "Z"];
+  console.log(alpha)
 
-//   numbs.sort((a, b) => a-b);
-//   numbs.sort((a, b) => b-a);
-//   console.log(numbs)
+  alpha.sort();
+  console.log(alpha)
+
+
+  const numbs = [9, 7, 6, 8, 15, 25, 35, 99, 5, 1, 3, 2, 4]
+  console.log(numbs)
+
+  numbs.sort((a, b) => a-b);
+  numbs.sort((a, b) => b-a);
+  console.log(numbs)
 
 
 //     //***** indexOf METHOD () ************** :
 
 
-//     const $number = [1, 4, 3, 7, 2, 9, 8, 2, 4, 7, 5, 2]
-//     console.log($number)
+    const $number = [1, 4, 3, 7, 2, 9, 8, 2, 4, 7, 5, 2]
+    console.log($number)
 
-//     const index1 = $number.indexOf(2)
-//     console.log(index1)
+    const index1 = $number.indexOf(2)
+    console.log(index1)
 
 
-//     const tutor = ["Abdullahi", "Naheem", "Anate", "Seyi", "Soliu"]
-//     console.log(tutor)
-//     const indexTt = tutor.indexOf("Anate")
-//     console.log(indexTt)
+    const tutor = ["Abdullahi", "Naheem", "Anate", "Seyi", "Soliu"]
+    console.log(tutor)
+    const indexTt = tutor.indexOf("Anate")
+    console.log(indexTt)
 
-//     const lastIndex = $number.lastIndexOf(2)
-//     console.log(lastIndex)
+    const lastIndex = $number.lastIndexOf(2)
+    console.log(lastIndex)
 
 
 
 
 // //***** LOOP () ************** :
 
-// const _names = ['Zainab', 'Baheera', 'Nafeesat'];
+const _names = ['Zainab', 'Baheera', 'Nafeesat'];
 
-// //***** ES5 */
+//***** ES5 */
 
-// for(let i = 0; i < _names.length; i++) {
-//     console.log(i, _names[i])
-// };
-
-
-// //****** ES6 */
-
-// for(const _name of _names) {
-//     console.log(_name)
-// }
+for(let i = 0; i < _names.length; i++) {
+    console.log(i, _names[i])
+};
 
 
-// //***** FOREACH METHOD () ************** :
+//****** ES6 */
 
-// _names.forEach(function(){});
-
-// _names.forEach((name, i, arr) => {
-// console.log(name.toUpperCase())
-// console.log(i)
-// console.log(arr)
-// });
-
-// let _$numbers = [2, 4, 3, 1, 2, 4];
-
-// _$numbers.forEach(function(number, i){
-//     console.log(number)
-// });
+for(const _name of _names) {
+    console.log(_name)
+}
 
 
-// const mammals = ["Lion", "Cow", "Dog", "Cat"];
-// mammals.forEach((mammals, i)=> console.log(mammals, i));
+// //***** FOREACH METHOD () ************** : This method is used to execute function which is taken as a parameter and 
+//applied for each value in the set, in insertion
+
+_names.forEach(function(){});
+
+_names.forEach((name, i, arr) => {
+console.log(name.toUpperCase())
+console.log(i)
+console.log(arr)
+});
+
+let _$numbers = [2, 4, 3, 1, 2, 4];
+
+_$numbers.forEach(function(number, i){
+    console.log(number)
+});
 
 
-// // EXAMPLE 3
-
-// let totalVal = 0;
-// const trans = [42, 45, 26, 4, 13, 16]
-
-// trans.forEach((tran)=>{
-//     console.log(totalVal, (totalVal += tran), tran)
-// })
+const mammals = ["Lion", "Cow", "Dog", "Cat"];
+mammals.forEach((mammals, i)=> console.log(mammals, i));
 
 
-// console.log(totalVal);
+// EXAMPLE 3
+
+let totalVal = 0;
+const trans = [42, 45, 26, 4, 13, 16]
+
+trans.forEach((tran)=>{
+    console.log(totalVal, (totalVal += tran), tran)
+})
+
+
+console.log(totalVal);
 
 
 
 // // EXAMPLE 4
-// _names .forEach((name)=> console.log(`Congratulation ${name} you una they smoke igbo`))
+ _names .forEach((name)=> console.log(`Congratulation ${name} you una they smoke igbo`))
 
 
 
@@ -267,57 +269,57 @@
 //  // of calling a provided function on every element in the calling array
    
 
-//  const  number = [1, 2, 3, 4, 5, 6];
-//     console.log(number)
-//     const numberDuble = number.map(double)
+ const  number = [1, 2, 3, 4, 5, 6];
+    console.log(number)
+    const numberDuble = number.map(double)
     
-//     const numberMuitiply = number.map(multiply)
+    const numberMuitiply = number.map(multiply)
 
-//     function double(value, index, array) {
-//         return value * 2
-//     }
+    function double(value, index, array) {
+        return value * 2
+    }
 
-//     function multiply(value, index, array) {
-//         return value * index
-//     }
+    function multiply(value, index, array) {
+        return value * index
+    }
 
-//     console.log(numberDuble)
-//     console.log(numberMuitiply)
+    console.log(numberDuble)
+    console.log(numberMuitiply)
 
 
 //     // EXAMPLES 2
 
-//     const inventory = [
-//         {name: "Rice", price: 4000},
-//         {name: "Beans", price: 4000},
-//         {name: "Semo", price: 4000},
-//         {name: "Yam", price: 4000},
-//         {name: "Garri", price: 4000}
-//     ];
+    const inventory = [
+        {name: "Rice", price: 4000},
+        {name: "Beans", price: 4000},
+        {name: "Semo", price: 4000},
+        {name: "Yam", price: 4000},
+        {name: "Garri", price: 4000}
+    ];
 
-    // inventory.map((value)=>{
-    //     console.log(value.name)
-    // })
+    inventory.map((value)=>{
+        console.log(value.name)
+    })
    
-        // const price = inventory.map((value) => {
-        //     return value.price
-        // });
-        // console.log(price)
+        const price = inventory.map((value) => {
+            return value.price
+        });
+        console.log(price)
         
-        // const name = inventory.map((value) => {
-        //     return value.name
-        // });
-        // console.log(name);
+        const name = inventory.map((value) => {
+            return value.name
+        });
+        console.log(name);
         
-        // const commodities = inventory.map((commodity) => commodity.name)
-        // console.log(commodities);
+        const commodities = inventory.map((commodity) => commodity.name)
+        console.log(commodities);
         
-        // let doublek = [2, 3, 4, 5, 6, 7];
+        let doublek = [2, 3, 4, 5, 6, 7];
         
-        // let $double = doublek.map((numb) => {
-        //     return numb * 2
-        // });
-        // console.log($double)
+        let $double = doublek.map((numb) => {
+            return numb * 2
+        });
+        console.log($double)
 
 
 
@@ -327,19 +329,16 @@
 
         const numbers = [-10, 0, -2, 4, -9, 5, -8]
 
-        // const filteredNumbers=numbers.filter((number) => {
-        //     return number < 0
+         const filteredNumbers=numbers.filter((number) => {
+             return number < 0
 
-        // })
+         })
 
-        // console.log(filteredNumbers)
-
-
+         console.log(filteredNumbers)
 
 
-// for(const filterdNumber of filteredNumbers) {
-//      console.log(filteredNumbers)
-//  }
+
+ for(const filterdNumber of filteredNumbers) {      console.log(filteredNumbers)  }
 
 
 const filteredNumbers1 = []
--- ObjectAndItsMethod/script.js
+++ ObjectAndItsMethod/script.js
@@ -3,20 +3,20 @@
 // value pairs
 
 //******** Examples ******/
-// const firstName = "Soliu"
+const firstName = "Soliu"
 
-// const person = {
-//     firstName,
-//     surname:   "Hayzed",
-//     age: 158,
-//     chhildren:
-//     {
-//         firstChild: "Eef",
-//         secondChild: "Nafisah"
-//     }
-// }
+const person = {
+    firstName,
+    surname:   "Hayzed",
+    age: 158,
+    chhildren:
+    {
+        firstChild: "Eef",
+        secondChild: "Nafisah"
+    }
+}
 
-// console.log(person)
+console.log(person)
 
 
 //**** ASSESSING AND ADDING OF PROPERTIES TO AN OBJECT */
@@ -38,7 +38,8 @@ console.log(human["DLT- africa location"])
 
 console.log(human)
 
-// OBJECT BUILT IN METHOD
+// OBJECT BUILT IN METHOD: This is used to access and make 
+// working with individual objects straightforward
 
 const pet = {
     petName: "dog",
@@ -60,3 +61,195 @@ const dog = {
 }
 
 dog.listAllProperties()
+
+
+// Another Example
+
+const myObject = {
+    myMethod1: () => {}, // arrow function
+    myMethod2: function () {}, //function declaration
+
+};
+
+
+const animal = {
+    name: "fluffy Orange",
+    age: 10,
+
+    bark: () => {
+        console.log("Woof Woof!")
+    }
+}
+
+console.log(animal.name)
+animal.bark()
+
+
+//***** OBJECT methods  */
+/// OBJECT.KEYS
+/// OBJECT.VALUE
+/// OBJECT.ENTRIES
+/// OBJECT.FREEZEES
+/// OBJECT.SEAL
+
+const employees = {
+        boss: "Soliu",
+        secretary: "Azeez",
+        sales: "Zainab",
+        accountant: "Bola",
+        cleaner: "Ade"
+}
+
+
+/// OBJECT.KEYS
+const employeeInfo = Object.keys(employees)
+console.log( employeeInfo)
+
+
+/// another example
+
+/// OBJECT.VALUE
+
+const session = {
+    id: 1,
+    date: "27th-feb-2024",
+    device: "Mobile",
+    browser: "chrome"
+}
+
+const sessionValue = Object.values(session)
+console.log(sessionValue)
+
+
+/// OBJECT.ENTRIES
+//******* Example */
+const operatingSystem = {
+    name: "Linux",
+    version: 18.04,
+    license: "Open source"
+}
+
+const newSystem = Object.entries(operatingSystem)
+console.log(newSystem)
+
+
+
+/// OBJECT.FREEZEES
+
+// Example
+
+const user = {
+    username: "John",
+    password: 12345,
+}
+
+const userInfo = Object.freeze(user)
+console.log(userInfo )
+
+userInfo.username = "soliu"
+// console.log(userInfo)
+
+
+/// OBJECT.SEAL
+
+//***** Example */
+
+const userOne ={
+    userName: "Joseph",
+    password: 12345
+}
+
+
+
+const userOneInfo = Object.seal(userOne)
+userOneInfo.userName = "Az"
+console.log(userOneInfo)
+
+
+
+// Deleting and updating object properties
+
+    let x
+    let y
+const perSon = {
+      name: "Hayzed",
+      age: 35,
+      isAdmin: true,
+
+      address: {
+        street: "Oju-oore",
+        city:   "Ota",
+        state:  "Ogun"
+      },
+
+      hobbies: ["Music", "Sport"]
+
+}
+
+
+// accessing an array inside an object
+
+
+x = perSon.hobbies[0]
+y = perSon.hobbies[1]
+// let y = perSon["hobbies"][1]
+console.log(x, y)
+
+
+// updating a property
+
+perSon.name = "Bode"
+console.log(perSon)
+
+
+perSon.address.street = "Oju-Elegba, ijebo-Ode"
+console.log(perSon)
+
+
+// Delete a property
+
+delete perSon.address.state
+console.log(perSon)
+
+
+// adding a function to an object
+
+perSon.greet = function() {
+    console.log(`Hello, my name is ${this.name}`)
+}
+
+perSon.greet()
+
+// Value and Reference: these are  divided into two namely
+// copying primitive value
+
+//copying numbers
+let x1 = 1
+let y1 = x1;
+
+x1 = 2
+
+console.log(x1)
+console.log(y1)
+
+
+// copy string
+
+let firstPerson = "Mark"
+let secondPerson = firstPerson
+
+firstPerson = "Austin"
+
+console.log(firstPerson)
+console.log(secondPerson)
+
+
+// copying complex valy(Non-primitive data): under is we have
+// object, array, daye.
+
+const animals = ["dogs", "cats"];
+const otherAnimals = animals
+
+animals.push("Goat");
+console.log(animals)
+console.log(otherAnimals)
